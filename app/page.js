3ed2cfff331cba5ff43e6cbd718ec3ce0671fa9d
a-nshul <upadhyay041298@gmail.com>
2024-09-17 15:59:19 +0530

    updated code

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 'use client';
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation'; // Import useRouter hook
 import LoginPage from '../components/login/Login';
 import ResumeForm from '../components/VcardForm';
 import ResumeTemplate from '../components/VcardTemplate';
+import { LogoutOutlined } from '@ant-design/icons';
 
 const HomePage = () => {
   const [resumeData, setResumeData] = useState(null);
   const [loggedIn, setLoggedIn] = useState(false);
+  const router = useRouter(); // Initialize useRouter
 
   const handleFormSubmit = (values) => {
     setResumeData(values);
@@ -17,22 +20,38 @@ const HomePage = () => {
     setLoggedIn(true);
   };
 
-  return (
-    <div className="container mx-auto p-4">
-      {loggedIn ? (
-        <div className="flex flex-col md:flex-row gap-4">
-          {/* Form Section with Reduced Width */}
-          <div className="flex-1 max-w-[600px] bg-gray-100 p-3 rounded-lg shadow-lg">
-            <ResumeForm onSubmit={handleFormSubmit} />
-          </div>
+  const handleLogout=()=>{
+    setLoggedIn(false);
+    window.location.reload();
+  }
 
-          {/* Template Section */}
-          {resumeData && (
-            <div className="flex-1 min-w-[250px] text-white p-4 rounded-lg shadow-lg">
-              <ResumeTemplate resumeData={resumeData} />
+  return (
+    <div className="relative container mx-auto p-4">
+      {loggedIn ? (
+        <>
+          {/* Logout Icon */}
+          <span 
+            onClick={handleLogout} 
+            className="absolute top-4 right-4 cursor-pointer text-red-500 hover:text-red-700"
+            title="Logout"
+          >
+            <LogoutOutlined style={{ fontSize: '24px' }} />
+          </span>
+
+          <div className="flex flex-col md:flex-row gap-4 mt-10">
+            {/* Form Section with Reduced Width */}
+            <div className="flex-1 max-w-[600px] bg-gray-100 p-3 rounded-lg shadow-lg">
+              <ResumeForm onSubmit={handleFormSubmit} />
             </div>
-          )}
-        </div>
+
+            {/* Template Section */}
+            {resumeData && (
+              <div className="flex-1 min-w-[250px] text-white p-4 rounded-lg shadow-lg">
+                <ResumeTemplate resumeData={resumeData} />
+              </div>
+            )}
+          </div>
+        </>
       ) : (
         <LoginPage onLoginSuccess={handleLoginSuccess} />
       )}
--- components/VcardForm.js
+++ components/VcardForm.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { Form, Input, Button, Typography, Divider, Upload } from 'antd';
+import { Form, Input, Button, Select,Typography, Divider, Upload, Row, Col,DatePicker } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import 'antd/dist/reset.css';
 
@@ -22,90 +22,91 @@ const ResumeForm = ({ onSubmit }) => {
         onFinish={handleFinish}
         className="space-y-4"
       >
-        <Form.Item
-          label="Cover Image"
-          name="coverImage"
-          valuePropName="fileList"
-          getValueFromEvent={({ fileList }) => fileList}
-          extra="Upload a cover image for your resume."
-        >
-          <Upload 
-            beforeUpload={() => false} 
-            listType="picture" 
-            className="upload-list-inline"
-            showUploadList={{ showPreviewIcon: true, showRemoveIcon: true }}
-          >
-            <Button icon={<UploadOutlined />}>Upload Cover Image</Button>
-          </Upload>
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="Cover Image"
+              name="coverImage"
+              valuePropName="fileList"
+              getValueFromEvent={({ fileList }) => fileList}
+              extra="Upload a cover image for your resume."
+            >
+              <Upload 
+                beforeUpload={() => false} 
+                listType="picture" 
+                className="upload-list-inline"
+                showUploadList={{ showPreviewIcon: true, showRemoveIcon: true }}
+              >
+                <Button icon={<UploadOutlined />}>Upload Cover Image</Button>
+              </Upload>
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Profile Image"
+              name="profileImage"
+              valuePropName="fileList"
+              getValueFromEvent={({ fileList }) => fileList}
+              extra="Upload a profile image for your resume."
+            >
+              <Upload 
+                beforeUpload={() => false} 
+                listType="picture" 
+                className="upload-list-inline"
+                showUploadList={{ showPreviewIcon: true, showRemoveIcon: true }}
+              >
+                <Button icon={<UploadOutlined />}>Upload Profile Image</Button>
+              </Upload>
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item
-          label="Profile Image"
-          name="profileImage"
-          valuePropName="fileList"
-          getValueFromEvent={({ fileList }) => fileList}
-          extra="Upload a profile image for your resume."
-        >
-          <Upload 
-            beforeUpload={() => false} 
-            listType="picture" 
-            className="upload-list-inline"
-            showUploadList={{ showPreviewIcon: true, showRemoveIcon: true }}
-          >
-            <Button icon={<UploadOutlined />}>Upload Profile Image</Button>
-          </Upload>
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: 'Please input your name!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Profession"
+              name="profession"
+              rules={[{ required: true, message: 'Please input your profession!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item
-          label="Name"
-          name="name"
-          rules={[{ required: true, message: 'Please input your name!' }]}
-        >
-          <Input />
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item label="Facebook" name="facebook">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Instagram" name="instagram">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item
-          label="Profession"
-          name="profession"
-          rules={[{ required: true, message: 'Please input your profession!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Facebook"
-          name="facebook"
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Instagram"
-          name="instagram"
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="LinkedIn"
-          name="linkedin"
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Whatsapp"
-          name="whatsapp"
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Other"
-          name="other"
-        >
-          <Input />
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item label="LinkedIn" name="linkedin">
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Whatsapp" name="whatsapp">
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
         <Divider />
 
@@ -119,79 +120,82 @@ const ResumeForm = ({ onSubmit }) => {
 
         <Divider />
 
-        <Form.Item
-          label="Email ID"
-          name="email"
-          rules={[{ type: 'email', message: 'The input is not valid E-mail!' }, { required: true, message: 'Please input your email!' }]}
-        >
-          <Input />
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="Email ID"
+              name="email"
+              rules={[{ type: 'email', message: 'The input is not valid E-mail!' }, { required: true, message: 'Please input your email!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Mobile Number"
+              name="mobile"
+              rules={[{ required: true, message: 'Please input your mobile number!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
-        <Form.Item
-          label="Mobile Number"
-          name="mobile"
-          rules={[{ required: true, message: 'Please input your mobile number!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Date of Birth"
-          name="dob"
-          rules={[{ required: true, message: 'Please input your date of birth!' }]}
-        >
-          <Input type="date" />
-        </Form.Item>
-
-        <Form.Item
-          label="Location"
-          name="location"
-          rules={[{ required: true, message: 'Please input your location!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Divider />
-
-        <Form.Item
-          label="Service 1"
-          name="service1"
-          rules={[{ required: true, message: 'Please input your first service!' }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Service 2"
-          name="service2"
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Service 3"
-          name="service3"
-        >
-          <Input />
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+          <Form.Item
+            label="Date of Birth"
+            name="dob"
+            rules={[{ required: true, message: 'Please input your date of birth!' }]}
+          >
+            <DatePicker
+              format="YYYY-MM-DD" // Customize the format if needed
+              style={{ width: '100%', fontSize: '16px' }} // Adjust the size
+              size="large" // Change the size to 'large' or any other size
+              inputReadOnly
+            />
+          </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Location"
+              name="location"
+              rules={[{ required: true, message: 'Please input your location!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
 
         <Divider />
 
-        <Form.Item
-          label="Appointment Date"
-          name="appointmentDate"
-          rules={[{ required: true, message: 'Please select an appointment date!' }]}
-        >
-          <Input type="date" />
-        </Form.Item>
+        
+        <Divider />
 
-        <Form.Item
-          label="Available Hours"
-          name="availableHours"
-          rules={[{ required: true, message: 'Please input available hours!' }]}
-        >
-          <Input />
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              label="Appointment Date"
+              name="appointmentDate"
+              rules={[{ required: true, message: 'Please select an appointment date!' }]}
+            >
+              <Input type="date" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+          <Form.Item
+            label="Available Hours"
+            name="availableHours"
+            rules={[{ required: true, message: 'Please select available hours!' }]}
+          >
+            <Select placeholder="Select available hours">
+              <Option value="12:00 PM">12:00 PM</Option>
+              <Option value="3:00 PM">3:00 PM</Option>
+              <Option value="6:00 PM">6:00 PM</Option>
+            </Select>
+          </Form.Item>
+          </Col>
+        </Row>
 
         <Form.Item>
           <Button type="primary" htmlType="submit" className="bg-blue-500 text-white">
@@ -199,6 +203,7 @@ const ResumeForm = ({ onSubmit }) => {
           </Button>
         </Form.Item>
       </Form>
+      
     </div>
   );
 };
